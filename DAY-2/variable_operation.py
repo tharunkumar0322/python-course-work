23c147c27bf4fc178eb1078235eb502480a182e5
Tharun Kumar Govindu <tharunkumar0322@gmail.com>
2026-08-20 23:48:03 +0530

    python intro

---
+++ DAY-2/variable_operation.py
@@ -0,0 +1,31 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=b=c=10
+>>> a
+10
+>>> b
+10
+>>> c
+10
+>>> a,b,c = 12,13,14
+>>> a
+12
+>>> b
+13
+>>> c
+14
+>>> #defining a variable
+>>> var=15
+>>> var
+15
+>>> 1var
+SyntaxError: invalid syntax
+>>> var1
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    var1
+NameError: name 'var1' is not defined
+>>> var1=10
+>>> var1
+10
+>>> 
